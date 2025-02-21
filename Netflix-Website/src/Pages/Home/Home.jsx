97803a47005687115e00bef33773dab5b58fc4ff
ruki1988 <rukiamarie5@gmail.com>
2025-02-20 21:34:21 -0500

    deploy

--- Netflix-Website/src/Pages/Home/Home.jsx
+++ Netflix-Website/src/Pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
 import Banner from '../../Components/Banner/Banner'
+import RowList from '../../Components/Rows/RowList/RowList'
 
 function Home() {
   return (
       <>
-      <Header />
+      <Header/>
       <Banner/>
-          <Footer/>
+      <RowList/>
+      <Footer/>
           
     </>
   )
--- Netflix-Website/src/utils/requests.jsx
+++ Netflix-Website/src/utils/requests.jsx
@@ -1,13 +1,25 @@
-const API_KEY = import.meta.env.VITE_API_KEY 
+const API_KEY = import.meta.env.VITE_MOVIE_DB_api_key; 
+// const requests = {
+//   FetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en_US`,
+//   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+//   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en_US`,
+//   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+//   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+//   fetchHorrorsMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+//   fetchRomancesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+//   fetchDocumentaryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+//   fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en_US`,
+// };
+// export default requests;
 const requests = {
-  FetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en_US`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en_US`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorsMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomancesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-  fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en_US`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 };
 export default requests;
